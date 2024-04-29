c84586e037db0d6d1856304da91d18bc8b47a512
LAPTOP-S7BN5M2L\Admin <huukhang3999@gmail.com>
2024-04-29 13:52:04 +0700

    main_UI done 60%

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.google.android.material:material:1.3.0-alpha03")
 
     implementation("androidx.room:room-runtime:2.6.1")
     annotationProcessor("androidx.room:room-compiler:2.6.1")
