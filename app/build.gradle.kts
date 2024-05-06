2b8e35412ba8780b4f1eb1b8f453979e1fef60f9
PhucTran <tranhieuphuc12@gmail.com>
2024-05-06 09:15:58 +0700

    add google services for firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,6 +73,7 @@ dependencies {
     implementation(libs.ui.graphics)
     implementation(libs.ui.tooling.preview)
     implementation(libs.material3)
+    implementation(libs.firebase.auth)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
--- app/src/main/java/vn/edu/tdc/rentaka/activities/MainActivity.java
+++ app/src/main/java/vn/edu/tdc/rentaka/activities/MainActivity.java
@@ -47,120 +47,121 @@ import vn.edu.tdc.rentaka.fragments.PersonalProfileFragment;
 public class MainActivity extends AppCompatActivity {
 
 // <<<<<<< future/confirm-rental-ui
-//     private DatabaseReference mDatabase;
+     private DatabaseReference mDatabase;
 
-//     @Override
-//     protected void onCreate(Bundle savedInstanceState) {
-//         super.onCreate(savedInstanceState);
-//         EdgeToEdge.enable(this);
-//         setContentView(R.layout.confirm_rental_layout);
+     @Override
+     protected void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+         EdgeToEdge.enable(this);
+         setContentView(R.layout.confirm_rental_layout);
 
-//         mDatabase = FirebaseDatabase.getInstance("https://rentaka-android-app-default-rtdb.asia-southeast1.firebasedatabase.app").getReference();
-// //        writeNewUser("1", "Alice", "Alice@email.com");
-//         ValueEventListener postListener = new ValueEventListener() {
-//             @Override
-//             public void onDataChange(DataSnapshot dataSnapshot) {
-//                 // Get Post object and use the values to update the UI
-// //                ArrayList<User> users = new ArrayList<>();
-// //                for (DataSnapshot userSnapshot : dataSnapshot.child("users").getChildren()) {
-// //                    User user = userSnapshot.getValue(User.class);
-// //                    users.add(user);
-// //                }
-// //                Log.d("Test", "onDataChange: "+users.toString());
-//                 User user = dataSnapshot.child("users").child("1").getValue(User.class);
-//                 Log.d("Test", "onDataChange: "+user.toString());
-//             }
+         mDatabase = FirebaseDatabase.getInstance("https://rentaka-android-app-default-rtdb.asia-southeast1.firebasedatabase.app").getReference();
+ //        writeNewUser("1", "Alice", "Alice@email.com");
+         ValueEventListener postListener = new ValueEventListener() {
+             @Override
+             public void onDataChange(DataSnapshot dataSnapshot) {
+                 // Get Post object and use the values to update the UI
+ //                ArrayList<User> users = new ArrayList<>();
+ //                for (DataSnapshot userSnapshot : dataSnapshot.child("users").getChildren()) {
+ //                    User user = userSnapshot.getValue(User.class);
+ //                    users.add(user);
+ //                }
+ //                Log.d("Test", "onDataChange: "+users.toString());
+                 User user = dataSnapshot.child("users").child("1").getValue(User.class);
+                 Log.d("Test", "onDataChange: "+user.toString());
+             }
 
-//             @Override
-//             public void onCancelled(DatabaseError databaseError) {
-//                 // Getting Post failed, log a message
-//                 Log.d("Test", "loadPost:onCancelled", databaseError.toException());
-//             }
-//         };
-//         mDatabase.addValueEventListener(postListener);
-//     }
-//     public void writeNewUser(String userId, String name, String email) {
-//         User user = new User(name, email);
+             @Override
+             public void onCancelled(DatabaseError databaseError) {
+                 // Getting Post failed, log a message
+                 Log.d("Test", "loadPost:onCancelled", databaseError.toException());
+             }
+         };
+         mDatabase.addValueEventListener(postListener);
+     }
+     public void writeNewUser(String userId, String name, String email) {
+         User user = new User(name, email);
 
-//         mDatabase.child("users").child(userId).child("username").setValue(name);
+         mDatabase.child("users").child(userId).child("username").setValue(name);
+     }
 // =======
     //Properties
-    private AbstractFragment fragment;
-    private int currentFragment = 0;
-    private MainLayoutBinding binding;
-    // doi tuong dung de dan fragment vao khung man hinh
-    private FragmentTransaction transaction;
-
-    // Set color when click
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_layout);
-
-        //Khoi tao binding
-        binding = MainLayoutBinding.inflate(getLayoutInflater());
-        // Gán view cho binding
-        setContentView(binding.getRoot());
-
-        fragment = new HomeFragment();
-        updateUI();
-
-        binding.bottomMenu.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                if (menuItem.getItemId() == R.id.homeItemMenu){
-                    currentFragment = 0;
-                } else if(menuItem.getItemId() == R.id.newsItemMenu){
-                    currentFragment = 1;
-                } else if(menuItem.getItemId() == R.id.historyItemMenu){
-                    currentFragment = 2;
-                } else if(menuItem.getItemId() == R.id.notificationItemMenu){
-                    currentFragment = 3;
-                } else if(menuItem.getItemId() == R.id.profileItemMenu){
-                    currentFragment = 4;
-                }
-                updateUI();
-                return true;
-            }
-        });
-
-    }
-
-    private void updateUI() {
-        // Set title
-        // Neu da ton tai thi tai su dung
-        if (getSupportFragmentManager().findFragmentByTag(currentFragment + "") != null) {
-            fragment = (AbstractFragment) getSupportFragmentManager().findFragmentByTag(currentFragment + "");
-        }
-        // Neu chua ton tai thi tao moi fragment
-        else {
-            // Tao doi tuong fragment tuong ung
-            if (currentFragment == 0) {
-                fragment = new HomeFragment();
-            } else if (currentFragment == 1) {
-                fragment = new NewsFragment();
-            } else if (currentFragment == 2) {
-                fragment = new HistoryFragment();
-            } else if (currentFragment == 3) {
-                fragment = new NotificationFragment();
-            } else if (currentFragment == 4) {
-                fragment = new PersonalProfileFragment();
-            }
-        }
-
-        // CHUAN BI CHO TRANSACTION
-        //Lay doi tuong fragment transaction
-        transaction = getSupportFragmentManager().beginTransaction();
-        // Do du lieu vao doi tuong va dan doi tuong fragment vao khung man hinh,
-        // voi tham so dau tien la id cua khung chua fragment o layout )
-        transaction.replace(R.id.fragmentContainer, fragment, currentFragment + "");
-        // Dua fragment vao trong Stack neu chua ton tai
-        if (getSupportFragmentManager().findFragmentByTag(currentFragment + "") == null) {
-            transaction.addToBackStack(null);
-        }
-        // Yeu cau thuc hien transaction
-        transaction.commit();
-
-    }
+//    private AbstractFragment fragment;
+//    private int currentFragment = 0;
+//    private MainLayoutBinding binding;
+//    // doi tuong dung de dan fragment vao khung man hinh
+//    private FragmentTransaction transaction;
+//
+//    // Set color when click
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.main_layout);
+//
+//        //Khoi tao binding
+//        binding = MainLayoutBinding.inflate(getLayoutInflater());
+//        // Gán view cho binding
+//        setContentView(binding.getRoot());
+//
+//        fragment = new HomeFragment();
+//        updateUI();
+//
+//        binding.bottomMenu.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+//                if (menuItem.getItemId() == R.id.homeItemMenu){
+//                    currentFragment = 0;
+//                } else if(menuItem.getItemId() == R.id.newsItemMenu){
+//                    currentFragment = 1;
+//                } else if(menuItem.getItemId() == R.id.historyItemMenu){
+//                    currentFragment = 2;
+//                } else if(menuItem.getItemId() == R.id.notificationItemMenu){
+//                    currentFragment = 3;
+//                } else if(menuItem.getItemId() == R.id.profileItemMenu){
+//                    currentFragment = 4;
+//                }
+//                updateUI();
+//                return true;
+//            }
+//        });
+//
+//    }
+//
+//    private void updateUI() {
+//        // Set title
+//        // Neu da ton tai thi tai su dung
+//        if (getSupportFragmentManager().findFragmentByTag(currentFragment + "") != null) {
+//            fragment = (AbstractFragment) getSupportFragmentManager().findFragmentByTag(currentFragment + "");
+//        }
+//        // Neu chua ton tai thi tao moi fragment
+//        else {
+//            // Tao doi tuong fragment tuong ung
+//            if (currentFragment == 0) {
+//                fragment = new HomeFragment();
+//            } else if (currentFragment == 1) {
+//                fragment = new NewsFragment();
+//            } else if (currentFragment == 2) {
+//                fragment = new HistoryFragment();
+//            } else if (currentFragment == 3) {
+//                fragment = new NotificationFragment();
+//            } else if (currentFragment == 4) {
+//                fragment = new PersonalProfileFragment();
+//            }
+//        }
+//
+//        // CHUAN BI CHO TRANSACTION
+//        //Lay doi tuong fragment transaction
+//        transaction = getSupportFragmentManager().beginTransaction();
+//        // Do du lieu vao doi tuong va dan doi tuong fragment vao khung man hinh,
+//        // voi tham so dau tien la id cua khung chua fragment o layout )
+//        transaction.replace(R.id.fragmentContainer, fragment, currentFragment + "");
+//        // Dua fragment vao trong Stack neu chua ton tai
+//        if (getSupportFragmentManager().findFragmentByTag(currentFragment + "") == null) {
+//            transaction.addToBackStack(null);
+//        }
+//        // Yeu cau thuc hien transaction
+//        transaction.commit();
+//
+//    }
 }
 
