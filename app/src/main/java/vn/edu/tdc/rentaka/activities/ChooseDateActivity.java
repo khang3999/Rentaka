a69d12701652dd3f845304ae6f447db9b46b9ce9
LAPTOP-S7BN5M2L\Admin <huukhang3999@gmail.com>
2024-05-09 15:20:37 +0700

    choose date activity 10%

--- app/src/main/java/vn/edu/tdc/rentaka/activities/ChooseDateActivity.java
+++ app/src/main/java/vn/edu/tdc/rentaka/activities/ChooseDateActivity.java
@@ -1,21 +1,34 @@
 package vn.edu.tdc.rentaka.activities;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
+import androidx.core.util.Pair;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.datepicker.MaterialDatePicker;
+
 import vn.edu.tdc.rentaka.R;
+import vn.edu.tdc.rentaka.databinding.ChooseDateLayoutBinding;
 
 public class ChooseDateActivity extends AppCompatActivity {
-
+    // Properties
+    private ChooseDateLayoutBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.choose_date_layout);
+        binding = ChooseDateLayoutBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.timeCheckin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
