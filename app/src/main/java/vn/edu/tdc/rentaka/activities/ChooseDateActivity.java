7ae4a37d29f5f6f44be0b3e55a4c4547c45a50a0
LAPTOP-S7BN5M2L\Admin <huukhang3999@gmail.com>
2024-05-09 11:24:48 +0700

    update UI

--- app/src/main/java/vn/edu/tdc/rentaka/activities/ChooseDateActivity.java
+++ app/src/main/java/vn/edu/tdc/rentaka/activities/ChooseDateActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -16,6 +17,5 @@ public class ChooseDateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.choose_date_layout);
-
     }
 }
