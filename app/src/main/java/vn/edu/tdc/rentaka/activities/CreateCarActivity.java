e1c84e500150bb8c17967d6e274329751d9c7bf6
charcasbeos <tranthianhthu0711@gmail.com>
2024-05-18 23:37:06 +0700

    done UI

--- app/src/main/java/vn/edu/tdc/rentaka/activities/CreateCarActivity.java
+++ app/src/main/java/vn/edu/tdc/rentaka/activities/CreateCarActivity.java
@@ -1,21 +1,40 @@
 package vn.edu.tdc.rentaka.activities;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import vn.edu.tdc.rentaka.R;
+import vn.edu.tdc.rentaka.databinding.CreateCarLayoutBinding;
 
 public class CreateCarActivity extends AppCompatActivity {
-
+    private ActivityResultLauncher<Intent> imagePickerLauncher;
+    private static final int STORAGE_PERMISSION_CODE = 101;
+    Uri imageUri;
+    private CreateCarLayoutBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.create_car_layout);
 
+        binding = CreateCarLayoutBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
+
+
 }
