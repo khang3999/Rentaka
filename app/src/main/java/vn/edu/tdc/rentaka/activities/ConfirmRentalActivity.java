bb0a62057713cec80dd924f4aedf8d945f1c5013
charcasbeos <tranthianhthu0711@gmail.com>
2024-06-04 19:15:44 +0700

    create confirm rental activity

---
+++ app/src/main/java/vn/edu/tdc/rentaka/activities/ConfirmRentalActivity.java
@@ -0,0 +1,26 @@
+package vn.edu.tdc.rentaka.activities;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import vn.edu.tdc.rentaka.databinding.ConfirmRentalLayoutBinding;
+import vn.edu.tdc.rentaka.databinding.ConfirmRentalOwnerLayoutBinding;
+import vn.edu.tdc.rentaka.databinding.RentalDetailLayoutBinding;
+
+public class ConfirmRentalActivity extends AppCompatActivity {
+    private ConfirmRentalLayoutBinding customerBinding;
+    private ConfirmRentalOwnerLayoutBinding owenBinding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Neu idUser == idOwner thi do man hinh ConfirmRentalOwnerLayoutBinding
+
+        //Nguoc lai thi do man hinh ConfirmRentalLayoutBinding
+
+
+        //Cho chon hinh
+    }
+}
